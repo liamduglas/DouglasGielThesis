8d8c9ea9ea36a759bfdb678defd4f8285fac8ea5
joshuagiel <joshuagiel@gmail.com>
2024-04-05 15:07:45 -0700

    delete commented lines and fix weight name

--- UNet.py
+++ UNet.py
@@ -5,14 +5,14 @@ import tensorflow as tf
 from keras import Model
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-import Data_Driver_Final as id
+import Data_Driver as dd
 from PlotLearning import PlotLearning
 import matplotlib
 
 callback = [PlotLearning()]
 
-x = id.images_x
-y = id.images_y
+x = dd.images_x
+y = dd.images_y
 
 seed = 42
 np.random.seed = seed
@@ -47,48 +47,33 @@ def UNet(inputs):
     c4 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b3)
     c4 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
     print("Shape c4: " + str(c4.shape))
-    p4 = tf.keras.layers.MaxPooling2D(pool_size=(2, 2))(c4)
-    b4 = tf.keras.layers.BatchNormalization()(p4)
 
+    u1 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c4)
+    cc1 = tf.keras.layers.concatenate([u1, c3], axis=3)
+    b4 = tf.keras.layers.BatchNormalization()(cc1)
     print("Shape b4: " + str(b4.shape))
-    c5 = tf.keras.layers.Conv2D(256, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b4)
-    c5 = tf.keras.layers.Conv2D(256, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
+    c5 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b4)
+    c5 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
     print("Shape c5: " + str(c5.shape))
 
-    u1 = tf.keras.layers.Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-    cc1 = tf.keras.layers.concatenate([u1, c4], axis=3)
-    b5 = tf.keras.layers.BatchNormalization()(cc1)
+    u2 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c5)
+    cc2 = tf.keras.layers.concatenate([u2, c2], axis=3)
+    b5 = tf.keras.layers.BatchNormalization()(cc2)
     print("Shape b5: " + str(b5.shape))
-    c6 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b5)
-    c6 = tf.keras.layers.Conv2D(128, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
+    c6 = tf.keras.layers.Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b5)
+    c6 = tf.keras.layers.Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
     print("Shape c6: " + str(c6.shape))
 
-    u2 = tf.keras.layers.Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-    cc2 = tf.keras.layers.concatenate([u2, c3], axis=3)
-    b6 = tf.keras.layers.BatchNormalization()(cc2)
+    u3 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c6)
+    cc3 = tf.keras.layers.concatenate([u3, c1], axis=3)
+    b6 = tf.keras.layers.BatchNormalization()(cc3)
     print("Shape b6: " + str(b6.shape))
-    c7 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b6)
-    c7 = tf.keras.layers.Conv2D(64, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
+    c7 = tf.keras.layers.Conv2D(16, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b6)
+    c7 = tf.keras.layers.Conv2D(16, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
     print("Shape c7: " + str(c7.shape))
 
-    u3 = tf.keras.layers.Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-    cc3 = tf.keras.layers.concatenate([u3, c2], axis=3)
-    b7 = tf.keras.layers.BatchNormalization()(cc3)
-    print("Shape b7: " + str(b7.shape))
-    c8 = tf.keras.layers.Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b7)
-    c8 = tf.keras.layers.Conv2D(32, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
-    print("Shape c8: " + str(c8.shape))
-
-    u4 = tf.keras.layers.Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-    cc4 = tf.keras.layers.concatenate([u4, c1], axis=3)
-    b8 = tf.keras.layers.BatchNormalization()(cc4)
-    print("Shape b8: " + str(b8.shape))
-    c9 = tf.keras.layers.Conv2D(16, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(b8)
-    c9 = tf.keras.layers.Conv2D(16, (2, 2), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
-    print("Shape c9: " + str(c9.shape))
-
-    outputs = tf.keras.layers.Conv2D(num_classes, (1, 1), activation='sigmoid')(c9)
-    print("Shape out: " + str(outputs.shape))
+    outputs = tf.keras.layers.Conv2D(num_classes, (1, 1), activation='sigmoid')(c7)
+    print("Output shape: " + str(outputs.shape))
     return outputs
 
 inputs = tf.keras.layers.Input(shape=(128, 128, 3))
@@ -100,19 +85,23 @@ model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.BinaryC
 x = x.squeeze()
 y = y.squeeze()
 
-x = x[0:400]
-y = y[0:400]
+# split training and testing
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1, random_state=80)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=1/9, random_state=80 )
 print(y_train.shape)
 print(x_train.shape)
 
-#model.fit(x_train, y_train, epochs=250, batch_size=32, validation_data=(x_val, y_val), use_multiprocessing=True, callbacks=[callback])
-#model.save_weights('final_weights_1channel/weights')
 
-model.load_weights('final_weights_1channel/weights')
+# train model
+model.fit(x_train, y_train, epochs=2, batch_size=32, validation_data=(x_val, y_val), callbacks=[callback])
+model.save_weights('weights.weights.h5')
+
+# model.load_weights('weights.weights.h5')
+
+# obtain predictions
 y_pred = model.predict(x_test)
 
+# discretize output masks
 dims = y_pred.shape
 rows = dims[1]
 col = dims[2]
@@ -122,8 +111,9 @@ for k in range(0, dims[0]):
         for j in range(0, col):
             y_pred[k, i, j] = np.round(y_pred[k, i, j])
 
-show_images = True
 
+# display images
+show_images = False
 if show_images:
     cmap = matplotlib.colors.ListedColormap(['green', 'white'])
     for i in range(0, int(20)):
@@ -133,12 +123,14 @@ if show_images:
         plt.show()
         plt.imshow(y_pred[i][:, :], interpolation='nearest', cmap=cmap, vmin=0, vmax=1)
         plt.show()
-        #time.sleep(5)
+        time.sleep(5)
 
 metric_y_test = y_test[:, :, :].flatten()
 metric_y_pred = y_pred[:, :, :].flatten()
-print(sklearn.metrics.f1_score(y_test[:, :, :].flatten(), y_pred[:, :, :].flatten()))
-print(sklearn.metrics.confusion_matrix(y_test[:, :, :].flatten(), y_pred[:, :, :].flatten()))
-print(sklearn.metrics.jaccard_score(metric_y_test, metric_y_pred))
 
-tf.keras.utils.plot_model(model, "model.png", show_shapes=True)
+# display metrics
+print("F1 Score:", sklearn.metrics.f1_score(y_test[:, :, :].flatten(), y_pred[:, :, :].flatten()))
+print("Confusion matrix:",sklearn.metrics.confusion_matrix(y_test[:, :, :].flatten(), y_pred[:, :, :].flatten()))
+print("Jaccard:",sklearn.metrics.jaccard_score(metric_y_test, metric_y_pred))
+
+# keras.utils.plot_model(model, "model.png", show_shapes=True)
